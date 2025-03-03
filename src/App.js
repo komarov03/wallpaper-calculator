1acb90ec96c99ffc01cad1e9a671b04013f931ca
Daniil Komarov <komarov.daniil20166@mail.ru>
2025-03-03 23:31:20 +0300

    gh-pages route fixed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import { CalculatorPromo } from "./pages/CalculatorPromo";
 import { Parameters } from "./pages/Parameters";
 
 function App() {
   return (
     <section className="calculator">
-      <BrowserRouter basename="/wallpaper-calculator">
+      <HashRouter>
         <Routes>
           <Route exact path="/" element={<CalculatorPromo />} />
           <Route path="/calculate" element={<Parameters />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </section>
   );
 }
